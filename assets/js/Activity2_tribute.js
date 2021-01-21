e17f515f4dfe3b83fe11930dcdce298c64b3f8ff
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-01-22 07:18:05 +0800

    updates

--- assets/js/Activity2_tribute.js
+++ assets/js/Activity2_tribute.js
@@ -1,21 +1,21 @@
 //getting the button
 var mybutton = document.getElementById("myBtn");
 
-// when user scrolls down 30px from the top of the document, show the button
+// when user scrolls down 400px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-if (document.body.scrollTop > 30 || document.documentElement.scrollTop >30) {
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
     mybutton.style.display = "block";
-} else {
+  } else {
     mybutton.style.display = "none";
-}
+  }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-document.body.scrollTop = 0;
-document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
 var counter = 1;
