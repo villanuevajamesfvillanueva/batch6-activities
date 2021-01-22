37f2fe8760c2aeb7f35c82954db861d2bcf7b9cc
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-01-22 21:52:49 +0800

    fixed footer, added company logos

--- assets/js/Activity2_tribute.js
+++ assets/js/Activity2_tribute.js
@@ -12,7 +12,7 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+// when the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
